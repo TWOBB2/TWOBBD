1e453e2dc18e1a9b528b035ff641818a343bd2a2
OliverDenis <denisrusnurislamov@gmail.com>
2024-09-13 13:04:07 +0300

    IDLE, YÜRÜME ve KOŞMA ANİM.
    animasyonlar eklendi ve kodlara açıklamaları yazıldı;
    IDLE+nefes alma,
    w ile yürüme,
    LEFT SHİFT ile KOŞMA animasyonları eklendi.

---
+++ Assets/animationStateController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class animationStateController : MonoBehaviour
+{
+    Animator animator;
+    int isWalkingHash;
+    int isRunningHash;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        isWalkingHash = Animator.StringToHash("isWalking");
+        isRunningHash = Animator.StringToHash("isRunning");
+    }
+
+    
+    void Update()
+    {
+        bool isRunning = animator.GetBool(isRunningHash);
+        bool isWalking = animator.GetBool(isWalkingHash);
+        bool forwardPressed = Input.GetKey("w");
+        bool runPressed = Input.GetKey("left shift");
+
+
+        // eğer oyuncu W basarsa
+        if (!isWalking && forwardPressed)
+        {
+            // isWalking Boolean aktif hale geliyor (kutucuk tikli)
+            animator.SetBool(isWalkingHash, true);
+        }
+        // Eğer oyuncu W basmazsa 
+        if (isWalking && !forwardPressed)
+        {   
+            // isWalking Boolean kapanır ve tekrar idle animasyonuna geçer
+            animator.SetBool(isWalkingHash, false); 
+        }
+
+        // eğer oyuncu yürüyorsa ve koşmuyorsa ve L Shifte basarsa
+        if (!isRunning && (forwardPressed && runPressed))
+        {
+            // isRunning Boolean aktif hale gelir
+            animator.SetBool (isRunningHash, true);
+        }
+
+        // eğer oyuncu KOŞUYORSA ve koşmayı yada yürümeyi bırakırsa
+        if (isRunning && (!forwardPressed || !runPressed))
+        {
+            // isRunning Boolean devredışı kalır
+            animator.SetBool(isRunningHash, false);
+        }
+
+
+    }
+}
