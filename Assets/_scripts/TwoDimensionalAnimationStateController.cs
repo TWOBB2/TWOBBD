32342d4c97f8393cc1828f06a2bbbf8b07d8083c
OliverDenis <denisrusnurislamov@gmail.com>
2024-09-15 03:07:15 +0300

    animasyon değişiklikleri
    bütün yünlerin animasyonları okey
    geriye gitme hariç
    koşma animasyonu eklenecek daha

---
+++ Assets/_scripts/TwoDimensionalAnimationStateController.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TwoDimensionalAnimationStateController : MonoBehaviour
+{
+    Animator animator;
+    float velocityZ = 0f;
+    float velocityX = 0f;
+    [SerializeField]
+    private float acceleration = 2.0f;
+    [SerializeField]
+    private float deceleration = 2.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        // aşağıdaki inputlar oyuncunun o tuşlara basmasına bağlı olarak "true" gerçeklerşir
+        bool forwardPressed = Input.GetKey("w");
+        bool leftPressed = Input.GetKey("a");
+        bool rightPressed = Input.GetKey("d");
+        bool backPressed = Input.GetKey("s");
+        bool runPressed = Input.GetKey("left shift");
+
+        // ileri doğru animasyon hızı artar
+        if (forwardPressed && velocityZ < 0.5f && !runPressed)
+        {
+            velocityZ += Time.deltaTime * acceleration;
+        }
+
+        // sola doğru artar
+        if (leftPressed && velocityX > -0.5f && !runPressed)
+        {
+            velocityX -= Time.deltaTime * acceleration;
+        }
+
+        // sağa doğru artar
+        if (rightPressed && velocityX < 0.5f && !runPressed)
+        {
+            velocityX += Time.deltaTime * acceleration;
+        }
+
+        // geriye doğru artar 
+        if (backPressed && velocityZ > -0.5f && !runPressed)
+        {
+            velocityZ += Time.deltaTime * acceleration;
+        }
+        
+        
+        // geriye gitmiyorsa hızını azaltma BOT OLMAZSA İŞARETİ DEĞİŞTİR
+        //if (!backPressed && velocityZ > 0.0f)
+        //{
+        //    velocityZ += Time.deltaTime * deceleration;
+        //}
+       
+
+        // velocityZ animasyon hızını azalt 
+        if (!forwardPressed && velocityZ > 0.0f)
+        {
+            velocityZ -= Time.deltaTime * deceleration;
+        }
+
+        // velocityZ hızını sıfırla
+        if (!forwardPressed && velocityZ < 0.0f)
+        {
+            velocityZ = 0.0f;
+        }
+
+        // sola doğru gitmiyorsa hız azaltma
+        if (!leftPressed && velocityX < 0.0f)
+        {
+            velocityX += Time.deltaTime * deceleration;
+        }
+
+        //  sağa doğru gitmiyorsa hız azaltma
+        if (!rightPressed && velocityX > 0.0f)
+        {
+            velocityX -= Time.deltaTime * deceleration;
+        }
+
+        
+
+        //velocityX hızını sıfırla
+        if (!leftPressed && !rightPressed && velocityX != 0.0f && (velocityX > -0.05f && velocityX < 0.05f))
+        {
+            velocityX = 0.0f;
+        }
+
+
+        animator.SetFloat("Velocity Z", velocityZ);
+        animator.SetFloat("Velocity X", velocityX);
+
+    }
+}
---
+++ Assets/_scripts/animationStateController.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class animationStateController : MonoBehaviour
+{
+    Animator animator;
+    int isWalkingHash;
+    int isRunningHash;
+    int isWalkingBackHash;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        isWalkingHash = Animator.StringToHash("isWalking");
+        isRunningHash = Animator.StringToHash("isRunning");
+        isWalkingBackHash = Animator.StringToHash("isWalkingBack");
+    }
+
+
+    void Update()
+    {
+        bool isWalkingBack = animator.GetBool(isWalkingBackHash);
+        bool isRunning = animator.GetBool(isRunningHash);
+        bool isWalking = animator.GetBool(isWalkingHash);
+        bool forwardPressed = Input.GetKey("w");
+        bool runPressed = Input.GetKey("left shift");
+        bool backPressed = Input.GetKey("s");
+
+
+        // eðer oyuncu W basarsa
+        if (!isWalking && forwardPressed)
+        {
+            // isWalking Boolean aktif hale geliyor (kutucuk tikli)
+            animator.SetBool(isWalkingHash, true);
+        }
+        // Eðer oyuncu W basmazsa 
+        if (isWalking && !forwardPressed)
+        {
+            // isWalking Boolean kapanýr ve tekrar idle animasyonuna geçer
+            animator.SetBool(isWalkingHash, false);
+        }
+
+        // eðer oyuncu yürüyorsa ve koþmuyorsa ve L Shifte basarsa
+        if (!isRunning && (forwardPressed && runPressed))
+        {
+            // isRunning Boolean aktif hale gelir
+            animator.SetBool(isRunningHash, true);
+        }
+
+        // eðer oyuncu KOÞUYORSA ve koşmayı yada yürümeyi býrakýrsa
+        if (isRunning && (!forwardPressed || !runPressed))
+        {
+            // isRunning Boolean devredýþý kalır
+            animator.SetBool(isRunningHash, false);
+        }
+
+        //eğer karakter geriye gitmiyorsa ve S tuşuna basarsa
+        if (!isWalkingBack && backPressed)
+        {
+            // geriye gitme animasyonu aktifleşir
+            animator.SetBool(isWalkingBackHash, true);
+        }
+
+        // Eğer Karakter geriye yürüyorsa ve yürümeyi bırakıyorsa
+        if (isWalkingBack && !backPressed)
+        {
+            // Idle pozisyonuna dönmüş olur
+            animator.SetBool(isWalkingBackHash, false);
+        }
+
+        // Eğer karakter geri yürüyorsa ve W tuşuna basarsa
+        if (isWalkingBack && forwardPressed)
+        {
+            // ileri yürüme animasyonu devreye girer
+            animator.SetBool(isWalkingHash, true);
+            
+        }
+
+        
+        
+        
+
+    }
+}
